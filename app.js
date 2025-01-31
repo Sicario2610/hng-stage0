da2b86b2b194b9613fa7feb8fa79938210eb4823
Abdulbasit Abdulrasheed <email>
2025-01-31 12:20:35 +0100

    new change

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const compression = require("compression");
-
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(compression());
-app.use(cors());
-app.use(express.json());
+app.use(compression()); // Compress responses to reduce payload size
+app.use(cors()); // Enable CORS for cross-origin requests
+app.use(express.json()); // Parse incoming JSON requests
 
 // Endpoint to Fetch Data
 app.get("/", (req, res) => {
     try {
         res.status(200).json({
             email: "abdulbasitabdulrasheed8@gmail.com",
-            current_datetime: new Date().toISOString(),
+            current_datetime: new Date().toISOString(), // Current date-time in ISO format
             github_url: "https://github.com/Sicario2610/hng-stage0.git"
         });
     } catch (err) {
