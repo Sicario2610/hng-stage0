55c629d27acc4b14bee37157294d137578cc357c
Abdulbasit Abdulrasheed <email>
2025-01-31 11:38:03 +0100

    using the compression package to make api faster

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const compression = require("compression");
+
 const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 
